a76fbe4b735f8eb034ef89dfe3f2f5e12b9b3912
Pedro312 <pedro47.596@gmail.com>
2017-02-27 15:36:55 -0800

    2/27

--- CSE7 - Pedro Cerda - World Map OOP.py
+++ CSE7 - Pedro Cerda - World Map OOP.py
@@ -10,7 +10,7 @@ class Room():
         self.down = D
     def move(self, direction):
             '''This function allows movement to a different node.
-            '''
+            '''                                        
             global node
             node = globals()[getattr(self.direction)]
             
@@ -19,16 +19,27 @@ mentr = Room('Mall Entrance', 'hw','food', 'elev', None, None, None, ' You are i
  the front mall entrance. Behind you are the\
  mall front doors, but they are nailed shut.')
  
-hw = Room('Hallway', 'ftl', 'jai;', 'hw2', 'mentr', None, None, ' It\'s a long hallway.')
+hw = Room('Hallway', 'ftl', 'jail', 'hw2', 'mentr', None, None, ' It\'s a long hallway.')
 
 food = Room('Foodcourt', None, 'wfr', 'mentr', 'bath', None, None, ' There are\
 tons of empty tables. The light is flickering.')
 
-elev = Room('Elevator', None, 'mentr', None, None, ' It\'s an elevator. The\
+elev = Room('Elevator', None, 'mentr', None, None, None, None, ' It\'s an elevator. The\
 power is down.')
 
-wfr = Room('Wet Floor', None, None, 'food', None, ' The floor is significantly\
+wfr = Room('Wet Floor', None, None, 'food', None, None, None, ' The floor is significantly\
 moist.')
+
+bath = Room('Bathroom', 'food', None, None, None, None, None, ' It\'s a bathroom. The stalls\
+are locked and the mirrors are shattered.')
+
+jail = Room('Mall Jail', None, None, 'hw', None, None, None, ' This is the mall jail. It is\
+extremely cold, and a badge is gleaming on the desk.')
+
+ftl = Room('Footlocker', None, None, None, 'hw', None, None, ' It\'s a store. There are\
+shoes thrown all over the ground and fairly large footprints,')
+
+hw2 = Room('Hallway', None, 'hw', None, None, None, None, ' It\'s a long hallway.')
 
 node = mentr
 
