2167fe6a69fd8a52cc971096ab1fb6286c58de6b
Pedro312 <pedro47.596@gmail.com>
2017-03-05 17:59:44 -0800

    3/5

--- CSE7 - Pedro Cerda - World Map OOP.py
+++ CSE7 - Pedro Cerda - World Map OOP.py
@@ -16,38 +16,92 @@ class Room():
             '''                                        
             global node
             node = globals()[getattr(self,direction)]         
-                    
-mentr = Room('Mall Entrance', 'hw','food', 'elev', None, None, None, ' You are in\
- the front mall entrance. Behind you are the\
+#Room1                    
+mentr = Room('Mall Entrance', 'hw','food', 'elev', None, None, None, ' You\
+ are in the front mall entrance. Behind you are the\
  mall front doors, but they are nailed shut.')
  
-hw = Room('Hallway', 'ftl', 'jail', 'hw2', 'mentr', None, None, ' It\'s a long hallway.')
+ #Room2
+hw = Room('Hallway', 'ftl', 'jail', 'hw2', 'mentr', None, None, ' It\'s a long \
+hallway.')
 
+#Room3
 food = Room('Foodcourt', None, 'wfr', 'mentr', 'bath', None, None, ' There are\
 tons of empty tables. The light is flickering.')
 
-elev = Room('Elevator', None, 'mentr', None, None, None, None, ' It\'s an elevator. The\
-power is down.')
+#Room4
+elev = Room('Elevator', None, 'mentr', None, None, None, None, ' It\'s\
+an elevator. The power is down.')
 
-wfr = Room('Wet Floor', None, None, 'food', None, None, None, ' The floor is significantly\
- moist.')
+#Room5
+wfr = Room('Wet Floor', None, None, 'food', None, None, None, ' The floor is \
+significantly moist.')
 
-bath = Room('Bathroom', 'food', None, None, None, None, None, ' It\'s a bathroom. The stalls\
-are locked and the mirrors are shattered.')
+#Room6
+bath = Room('Bathroom', 'food', None, None, None, None, None, ' It\'s a \
+bathroom. The stalls are locked and the mirrors are shattered.')
 
-jail = Room('Mall Jail', None, None, 'hw', None, None, None, ' This is the mall jail. It is\
-extremely cold, and a badge is gleaming on the desk.')
+#Room7
+jail = Room('Mall Jail', None, None, 'hw', None, None, None, ' This is \
+the mall jail. It is extremely cold, and a badge is gleaming on the desk.')
 
-ftl = Room('Footlocker', None, None, None, 'hw', None, None, ' It\'s a store. There are\
-shoes thrown all over the ground and fairly large footprints,')
+#Room8
+ftl = Room('Footlocker', None, None, None, 'hw', None, None, ' It\'s a \
+store. There are shoes thrown all over the ground and fairly large footprints,')
 
-hw2 = Room('Hallway', 'pp', 'hw', None, None, None, None, ' It\'s a long hallway.')
+#Room9
+hw2 = Room('Hallway', 'pp', 'hw', 'pa', 'hg', None, None, ' It\'s a \
+long hallway.')
 
+#Room10
 pp = Room('Pretzel Palace', 'kc', None, None, 'hw2', None, None,'There is a cold\
  pretzel on the counter, and the cash register is empty.')
  
+ #Room11
 kc = Room('Kitchen', 'frz', None, None, 'pp', None, None, "It's a kitchen. There\
  is a freezer towards the back and pans on the ground.")
+
+#Room12
+hg = Room('Hunting Goods', 'hw2', None, None, 'ws', None, None,\
+"It's a hunting shop. There are firearms hung on the walls and on the counters.")
+
+#Room13
+ws = Room('Weapon Storage', 'hg', None, None, None, None, None, "There are\
+ racks of weapons on the walls and alligned on shelves, and stacks of\
+ ammunition in the corner of the room.")
+ 
+#Room14
+pa = Room('Play Area', 'ts', 'hw2', 'hw3', 'jwr', None, None, "There are\
+ multiple obstacle courses for children, but a few are broken in half\
+ and most have spider webs.")
+ 
+#Room15
+frz = Room('Freezer', None, None, None, 'kc', None, None, "It is extremely\
+ cold (obviously, it's a freezer) and to your right there are frozen\
+ water bottles.")
+ 
+#Room16
+ts = Room('Toy Store', None, None, None, 'pa', None, None, "This room seems to\
+ be oddly clean, compared to the rest. Although some shelves are still snapped.")
+ 
+#Room17
+jwr = Room('Jewelry Store', 'pa', None, None, None, None, None, "There are\
+ diamond rings in the glass cases, and a sparkling diamond necklace\
+ sitting alone on a counter top.")
+ 
+#Room18
+hw3 = Room('Hallway', 'fs', 'pa', 'hbp', 'co', None, None, "It's \
+the end of a hall the hall continues ahead.")
+ 
+#Room19
+hbp = Room('Hli\'s Beauty Products', None, 'hw3', None, None, None, None, "A\
+ very neat beauty store, with makeup products on  the shelves,\
+ and clothing hanging on racks organized by color. A nametag reading 'Hli'\
+ is lying on the counter.")
+ 
+#Room20
+co = Room('Clothing Outlet', 'hw3', None, None, None, None, None, "There is \
+tons of clothes thrown on the ground, and all the metal racks are flipped over.")
 
 node = mentr
 
